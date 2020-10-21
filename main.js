005500ce14f7f1db16397a7b3343e3e40fe7cfae
Wilder León <wilder_eduardo85@hotmail.com>
2020-10-21 18:01:50 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,46 @@
+$(document).ready(()=>{
+    $.getJSON('datos.json', (data)=> {
+        let clientes = data;
+        
+        $('#formulario').submit((e)=>{
+            e.preventDefault();
+            const busqueda = $('#busqueda').val();
+
+            const resultado = clientes.filter((item)=>{
+                return (item.rut === busqueda);
+            });
+
+            if (Object.keys(resultado).length>=1){
+                $.each(resultado, function (index, item) { 
+                    let documentos = item.documentos;
+                    $('#validacion').css( 'visibility', 'hidden');
+                    $('#contenedor').html('');
+                    $.each(documentos, function(index, item){
+                        $('#contenedor').html($('#contenedor').html()+`
+       
+                        <div class="caja">
+                            <img src="img/PDF.svg" alt="">
+                            <a href="${item}" target="_blank">Ver ${index}</a>
+                            <a href="${item}" download>Descargar</a>
+                        </div> 
+                       
+                        `);
+                    })
+                });
+            }else{
+                $('#validacion').css( 'visibility', 'visible');
+            }
+
+          
+            
+           
+
+            
+
+        
+        })
+        
+        }).fail(()=> {
+            console.log('Error al leer archivo JSON');
+        });
+})
