f6cf820559a027282776a248f10dd35a7ea30ab2
Wilder León <wilder_eduardo85@hotmail.com>
2020-10-22 10:01:22 -0300

    Se agregó botón de búsqueda al input y ahora aparecen los datos de cada cliente en el header

--- main.js
+++ main.js
@@ -11,33 +11,28 @@ $(document).ready(()=>{
             });
 
             if (Object.keys(resultado).length>=1){
+                
+                $('#cliente').text(resultado[0].nombres + ' ' + resultado[0].apellidos);
+
                 $.each(resultado, function (index, item) { 
                     let documentos = item.documentos;
                     $('#validacion').css( 'visibility', 'hidden');
                     $('#contenedor').html('');
                     $.each(documentos, function(index, item){
                         $('#contenedor').html($('#contenedor').html()+`
-       
+        
                         <div class="caja">
                             <img src="img/PDF.svg" alt="">
                             <a href="${item}" target="_blank">Ver ${index}</a>
                             <a href="${item}" download>Descargar</a>
                         </div> 
-                       
+                        
                         `);
                     })
                 });
             }else{
                 $('#validacion').css( 'visibility', 'visible');
             }
-
-          
-            
-           
-
-            
-
-        
         })
         
         }).fail(()=> {
